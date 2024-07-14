36aa6cc2d5361e87f2de4e99cd32e40d9c7c2d77
appleflyer <theappleflyer@icloud.com>
2024-07-14 20:44:22 +0800

    add stuffs

---
+++ public/bodycode.js
@@ -0,0 +1,42 @@
+const navbar = document.querySelector('.navbar');
+const notch = document.querySelector('.notch');
+const frame = document.getElementById('uv-frame');
+let hideTimer;
+let isIframeLoaded = false;
+
+// yummy event listeners 
+frame.addEventListener('load', () => {
+    isIframeLoaded = true;
+    navbar.classList.add('hidden');
+});
+
+notch.addEventListener('mouseenter', () => {
+    if (isIframeLoaded) {
+        navbar.classList.remove('hidden');
+        startHideTimer();
+    }
+});
+
+navbar.addEventListener('mouseenter', () => {
+    clearTimeout(hideTimer);
+});
+
+navbar.addEventListener('mouseleave', startHideTimer);
+
+function startHideTimer() {
+    clearTimeout(hideTimer);
+    hideTimer = setTimeout(() => {
+        if (isIframeLoaded) {
+            navbar.classList.add('hidden');
+        }
+    }, 500);
+}
+
+document.addEventListener('touchstart', (e) => {
+    if (e.touches[0].clientY < navbar.offsetHeight) {
+        if (isIframeLoaded) {
+            navbar.classList.remove('hidden');
+            startHideTimer();
+        }
+    }
+});
