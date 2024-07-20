1a8486fcaeac23e558a0ac93dc00cac4306ed8d4
liujustin604 <liujustin604@gmail.com>
2024-07-20 15:40:39 -0400

    add basic omnibox

--- public/index.js
+++ public/index.js
@@ -22,23 +22,36 @@ const errorCode = document.getElementById("uv-error-code");
 const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
 
 form.addEventListener("submit", async (event) => {
-	event.preventDefault();
+    event.preventDefault();
 
-	try {
-		await registerSW();
-	} catch (err) {
-		error.textContent = "Failed to register service worker.";
-		errorCode.textContent = err.toString();
-		throw err;
-	}
+    let frame = document.getElementById("uv-frame");
+    let search_bar = document.getElementById("uv-form");
+    let is_search_bar = search_bar.dataset.isSearchBar === "true";
 
-	const url = search(address.value, searchEngine.value);
+    try {
+        await registerSW();
+    } catch (err) {
+        error.textContent = "Failed to register service worker.";
+        errorCode.textContent = err.toString();
+        throw err;
+    }
 
-	let frame = document.getElementById("uv-frame");
-	frame.style.display = "block";
-	let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
-	if (await connection.getTransport() !== "/epoxy/index.mjs") {
-		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
-	}
-	frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+    const url = search(address.value, searchEngine.value);
+
+    if (is_search_bar) {
+        let proxied_wrapper = document.getElementById("proxied-content-wrapper");
+        proxied_wrapper.classList.replace("none", "fullscreen");
+
+
+        let omnibox_wrapper = document.getElementById("omnibox-wrapper");
+        omnibox_wrapper.appendChild(search_bar);
+        search_bar.classList.add("omnibox");
+        search_bar.dataset.isSearchBar = "false";
+    }
+
+    let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+    if (await connection.getTransport() !== "/epoxy/index.mjs") {
+        await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+    }
+    frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
